0ab73d2d3c7da3a29304644192c28d288927dadf
Mark <learncode23@gmail.com>
2022-01-17 16:05:17 +0000

    Added Keyboard Controls

--- tetris.js
+++ tetris.js
@@ -12,11 +12,65 @@ const matrix = [
   [0, 1, 0],
 ];
 
-matrix.forEach((row, y) => {
-  row.forEach((value, x) => {
-    if (value !== 0) {
-      context.fillStyle = 'red';
-      context.fillRect(x, y, 1, 1);
-    }
+function collide() {
+  // Will take a game and player
+}
+
+function draw() {
+  drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset) {
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value !== 0) {
+        context.fillStyle = 'red';
+        context.fillRect(x + offset.x, y + offset.y, 1, 1);
+      }
+    });
   });
+}
+
+function playerRotate(dir) {
+  // Take the current position and ++
+  // Need to add a collide function
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+let lastTime = 0;
+function update(time = 0) {
+  const deltaTime = time - lastTime;
+  lastTime = time;
+
+  dropCounter += deltaTime;
+  if (dropCounter > dropInterval) {
+    player.pos.y++;
+    dropcounter = 0;
+  }
+
+  draw();
+  requestAnimationFrame(update);
+}
+
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowLeft') {
+    playerMove(-1);
+  } else if (event.key === 'ArrowRight') {
+    playerMove(1);
+  } else if (event.key === 'ArrowDown') {
+    playerDrop();
+  } else if (event.key === 'q') {
+    playerRotate(-1);
+  } else if (event.key === 'w') {
+    playerRotate(1);
+  }
 });
+
+const player = {
+  pos: { x: 5, y: 5 },
+  matrix: matrix,
+};
+
+update();
